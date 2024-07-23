a11705eda27c4a0564a3d816d0b0604686b2fc04
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-07-23 22:28:53 +1000

    add the reports controller and index views

---
+++ app/controllers/desktop/reports_controller.rb
@@ -0,0 +1,5 @@
+module Desktop
+  class ReportsController < DesktopController
+    def index; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -86,4 +86,5 @@ Rails.application.routes.draw do
   get "/desktop/users", to: "desktop/users#index", as: "desktop_users"
   get "/desktop/users/:id", to: "desktop/users#show", as: "desktop_user"
   get "/desktop/retail", to: "desktop/retail#index", as: "desktop_retail"
+  get "/desktop/reports", to: "desktop/reports#index", as: "desktop_reports"
 end
