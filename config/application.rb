a00a6601fb97a9fc2977fda1b20fe402baecc1fe
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-04 22:24:52 +1000

    i think this config fixes my broken deploys

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module Tallii
     config.eager_load_paths << Rails.root.join("lib")
 
     config.generators.system_tests = nil
+
+    config.active_support.default_message_encryptor_serializer = :marshal
   end
 end
