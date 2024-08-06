8d9f11e169fc28ddd43de51c15f434e1ef32a137
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-06 23:39:22 +1000

    tweak importmap config to accept the file path

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,4 @@
 import { application } from "controllers/application"
 
-// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-// eagerLoadControllersFrom("controllers", application)
 import CheckoutController from "controllers/checkout_controller";
-application.register("checkout", CheckoutController);    
+application.register("checkout", CheckoutController);
