d6848b849dc99c40e9bef8c42482eef2e484ef7d
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-06 23:49:23 +1000

    this might only work on prod now

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
-import { application } from "controllers/application"
+import { application } from "./application"
 
 import CheckoutController from "./checkout_controller"
 application.register("checkout", CheckoutController)
