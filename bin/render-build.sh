d0d420b57c5d2fbc76e584399efbaef05117b02a
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-06 22:06:16 +1000

    skip step for now

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,8 +7,6 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 bin/rails db:environment:set RAILS_ENV=production
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bin/rails db:drop
 bin/rails db:create
 bin/rails db:migrate
 bin/rails db:seed
