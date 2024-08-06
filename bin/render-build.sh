4420de0e1a34dccfa85dc5f4bed72fa40a901503
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-06 22:04:54 +1000

    add safety check

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,7 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 bin/rails db:environment:set RAILS_ENV=production
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bin/rails db:drop
 bin/rails db:create
 bin/rails db:migrate
