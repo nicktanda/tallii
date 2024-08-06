6b341c7324b81e0603db34ad5d2b9e36f97bb495
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-06 23:33:12 +1000

    change path routing to be a bit more direct and not impacted by shit routes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,6 @@
 import { application } from "controllers/application"
 
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
+import CheckoutController from "controllers/checkout_controller";
+application.register("checkout", CheckoutController);    
