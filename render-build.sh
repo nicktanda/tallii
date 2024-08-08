ed37cbfb06d928aa27d4b3bd3765d43660a73764
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-08 22:00:37 +1000

    fix some weird environment setup issues

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,9 @@
 # exit on error
 set -o errexit
 
+bundle config unset force_ruby_platform
+bundle config set --local force_ruby_platform false
+
 bundle install
 bundle exec rails assets:precompile
 bunde exec rails assets:clean
