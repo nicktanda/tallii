b6cbba36942d9a6075bbaa067ebefbeaa8201322
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-08 22:11:53 +1000

    fix spelling error

--- render-build.sh
+++ render-build.sh
@@ -6,7 +6,7 @@ bundle config set --local force_ruby_platform false
 
 bundle install
 bundle exec rails assets:precompile
-bunde exec rails assets:clean
+bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 bundle exec rails db:seed
