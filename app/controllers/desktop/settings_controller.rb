d0c2855088c33f416c0dc76b14e8954aef4447e7
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-08-13 22:34:15 +1000

    add org settings that update

--- app/controllers/desktop/settings_controller.rb
+++ app/controllers/desktop/settings_controller.rb
@@ -16,13 +16,23 @@ module Desktop
 
     def organisation; end
     def update_organisation
+      current_organisation.assign_attributes(organisation_params)
 
+      if current_organisation.save
+        redirect_to desktop_settings_path, notice: 'Organisation updated'
+      else
+        redirect_back fallback_location: desktop_organisation_settings_path, alert: 'Invalid organisation information'
+      end
     end
 
     private
 
     def user_params
       params.require(:user).permit(:id, :first_name, :last_name, :email, :password, :phone, :weight, :address, :city, :postcode)
+    end
+
+    def organisation_params
+      params.require(:organisation).permit(:id, :name, :email, :phone, :address, :city, :postcode)
     end
   end
 end
