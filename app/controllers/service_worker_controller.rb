b9cbbbe92115d23099609e4b3195ec3612fc74cd
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-09-01 22:10:09 +1000

    fix service worker registry issue with skip action

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -2,6 +2,7 @@ class ServiceWorkerController < ApplicationController
   protect_from_forgery except: :service_worker
 
   skip_before_action :require_authenticated_user
+  skip_before_action :require_pet
 
   def service_worker
   end
