8825a3556c044af09eb530e4f0b527e67ecd9d7c
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-09-03 20:56:19 +1000

    fix indenting to see if that fixes the issue

--- app/javascript/custom/companion.js
+++ app/javascript/custom/companion.js
@@ -9,19 +9,19 @@ if (navigator.serviceWorker) {
       }
     }).then(() => console.log("[Companion]", "Service worker registered!"));
 
-    // This variable will save the event for later use.
-    let deferredPrompt;
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevents the default mini-infobar or install dialog from appearing on mobile
-      e.preventDefault();
-      // Save the event because you'll need to trigger it later.
-      deferredPrompt = e;
-      // Show your customized install prompt for your PWA
-      // Your own UI doesn't have to be a single element, you
-      // can have buttons in different locations, or wait to prompt
-      // as part of a critical journey.
-      showInAppInstallPromotion();
-    });
+  // This variable will save the event for later use.
+  let deferredPrompt;
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevents the default mini-infobar or install dialog from appearing on mobile
+    e.preventDefault();
+    // Save the event because you'll need to trigger it later.
+    deferredPrompt = e;
+    // Show your customized install prompt for your PWA
+    // Your own UI doesn't have to be a single element, you
+    // can have buttons in different locations, or wait to prompt
+    // as part of a critical journey.
+    showInAppInstallPromotion();
+  });
 
   function showInAppInstallPromotion() {
     // Example: Show an install button
