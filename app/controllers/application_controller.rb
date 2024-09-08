a041287023b71d3a3fe160d4fc9e153bc8338f25
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-09-08 20:58:18 +1000

    fix the weird session bug i was seeing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
   end
 
   def current_pet
-    return if session["current_pet"].nil?
-    @current_pet ||= current_user.pets.find_by(id: session["current_pet"])
+    return if session["pet"].nil?
+    @current_pet ||= current_user.pets.find_by(id: session["pet"])
   end
 
   def current_organisation
