36f1a6546d10b29a67964280914328aa359c1768
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-09-23 15:54:41 +1000

    add summary to the calendar widget

--- app/controllers/desktop/dashboard_controller.rb
+++ app/controllers/desktop/dashboard_controller.rb
@@ -2,9 +2,9 @@ module Desktop
   class DashboardController < DesktopController
     def index
       @date = params[:date] ? Date.new(params[:date].split("-").first.to_i, params[:date].split("-").second.to_i, params[:date].split("-").last.to_i) : Date.today
-      grooms = current_organisation.grooms.on_date(@date) + current_organisation.temporary_grooms.on_date(@date)
-      daycare_visits = current_organisation.daycare_visits.on_date(@date) + current_organisation.temporary_daycare_visits.on_date(@date)
-      @bookings = (grooms + daycare_visits).sort_by(&:time)
+      @grooms = current_organisation.grooms.on_date(@date) + current_organisation.temporary_grooms.on_date(@date)
+      @daycare_visits = current_organisation.daycare_visits.on_date(@date) + current_organisation.temporary_daycare_visits.on_date(@date)
+      @bookings = (@grooms + @daycare_visits).sort_by(&:time)
     end
   end
 end
