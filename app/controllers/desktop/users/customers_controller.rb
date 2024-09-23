480e06e33569d0f05ff02dc6cf792cc34d6c8abc
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2024-09-23 22:13:26 +1000

    add rewards points to user profile

--- app/controllers/desktop/users/customers_controller.rb
+++ app/controllers/desktop/users/customers_controller.rb
@@ -9,10 +9,10 @@ module Desktop
         @user = users.find(params[:id])
         @bookings = @user.pets.map do |pet|
           grooms = pet.grooms.map do |groom|
-            { pet: pet.name, time: groom.time, date: groom.date, status: groom.status }
+            { pet: pet.name, time: groom.time, date: groom.date, status: groom.formatted_status }
           end
           daycare_visits = pet.daycare_visits.map do |daycare_visit|
-            { pet: pet.name, time: daycare_visit.time, date: daycare_visit.date, status: daycare_visit.status }
+            { pet: pet.name, time: daycare_visit.time, date: daycare_visit.date, status: daycare_visit.formatted_status }
           end
           (grooms + daycare_visits).sort_by {|booking| [booking[:date], booking[:time]] }
         end.flatten
