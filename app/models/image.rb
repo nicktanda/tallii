dddeb9391d77246d579fbc62ec9e0712d34f8af4
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2025-01-08 13:27:40 +1000

    remove file size validation because thats stoopid

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,21 +9,13 @@ class Image < ApplicationRecord
   has_one_attached :image
 
   validate :correct_image_type
-  validate :image_size
 
   private
 
   def correct_image_type
     if image.attached? && !image.content_type.in?(%w(image/jpeg image/png image/gif image/webp))
       image.purge
-      errors.add(:image, 'must be a JPEG or PNG')
-    end
-  end
-
-  def image_size
-    if image.attached? && (image.byte_size <= 1.kilobyte || image.byte_size >= 2.megabytes)
-      image.purge
-      errors.add(:image, 'File size must be between 1KB and 2MB')
+      errors.add(:image, 'must be a JPEG, PNG, GIF or WEBP')
     end
   end
 end
