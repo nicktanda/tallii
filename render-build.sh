c2e0bf67e323bd57f7a98531d69161e5d574e684
nicktanda <nick.havilah@tanda.co>
2025-01-16 16:07:02 +1000

    fix bundle maybe

--- render-build.sh
+++ render-build.sh
@@ -3,7 +3,8 @@ set -o errexit
 
 bundle config unset force_ruby_platform
 bundle config set --local force_ruby_platform false
-gem install nokogiri --platform=ruby
+bundle config set force_ruby_platform true
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
