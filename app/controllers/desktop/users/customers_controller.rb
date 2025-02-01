1e86aaefe0a32a16b098364dd00451fefc01fd6e
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2025-02-01 12:58:59 +1000

    add customer edit path new design

--- app/controllers/desktop/users/customers_controller.rb
+++ app/controllers/desktop/users/customers_controller.rb
@@ -7,27 +7,28 @@ module Desktop
 
       def index
         @users = if params[:phone].present?
-          current_organisation.users.where("phone like ?", "%#{params[:phone]}%").where(role: "customer")
+          current_organisation.users.customers.where("phone like ?", "%#{params[:phone]}%")
         else
-          current_organisation.users.where(role: "customer")
+          current_organisation.users.customers
         end
       end
 
       def show
         @user = users.find(params[:id])
-        @bookings = @user.pets.map do |pet|
-          grooms = pet.grooms.map do |groom|
-            { pet: pet.name, time: groom.time, date: groom.date, status: groom.formatted_status }
-          end
-          daycare_visits = pet.daycare_visits.map do |daycare_visit|
-            { pet: pet.name, time: daycare_visit.time, date: daycare_visit.date, status: daycare_visit.formatted_status }
-          end
-          (grooms + daycare_visits).sort_by {|booking| [booking[:date], booking[:time]] }
-        end.flatten
+        @users = if params[:phone].present?
+          current_organisation.users.customers.where("phone like ?", "%#{params[:phone]}%")
+        else
+          current_organisation.users.customers
+        end
       end
 
       def edit
         @user = users.find(params[:id])
+        @users = if params[:phone].present?
+          current_organisation.users.customers.where("phone like ?", "%#{params[:phone]}%")
+        else
+          current_organisation.users.customers
+        end
       end
 
       def booking_settings
