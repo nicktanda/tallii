4408b9e9421fdd35ccb24777bcc3355313008064
nicktanda <nick.havilah@tanda.co>
2025-04-28 22:42:11 +1000

    lets try this

--- Gemfile
+++ Gemfile
@@ -55,7 +55,7 @@ gem 'stripe'
 
 gem "chartkick"
 
-gem "nokogiri"
+gem "nokogiri", "~> 1.18", platforms: [:ruby]
 
 gem "matrix", "~> 0.4.2"
 
--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,7 @@
 # exit on error
 set -o errexit
 
-bundle config unset force_ruby_platform
-bundle config set --local force_ruby_platform false
+bundle config set force_ruby_platform true
 
 bundle install
 bundle exec rails assets:precompile
