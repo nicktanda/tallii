951f1b3ae0b234a9ea431e4d70d6144d7c594792
nicktanda <nick.havilah@tanda.co>
2025-04-28 22:57:38 +1000

    remove this and see whats what

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,6 @@
 # exit on error
 set -o errexit
 
-bundle lock --add-platform x86_64-linux
-bundle config set force_ruby_platform true
-
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
