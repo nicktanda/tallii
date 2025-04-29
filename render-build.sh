b946c50a8cf93230dc884e4070bcdaa95c60f4cf
nicktanda <nick.havilah@tanda.co>
2025-04-29 22:37:58 +1000

    i think this might work

--- render-build.sh
+++ render-build.sh
@@ -4,10 +4,8 @@ set -o errexit
 bundle config set force_ruby_platform true
 bundle lock --add-platform x86_64-linux
 
-gem install bundler
-bundle update --bundler
-
 bundle install
+bundle update --bundler
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
