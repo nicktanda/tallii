a580a0f18efec40629df57b39b75ce036d52e006
nicktanda <nick.havilah@tanda.co>
2025-04-29 22:26:21 +1000

    precompile some more shit

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,13 @@
-# exit on error
 set -o errexit
 
 bundle config set force_ruby_platform true
 bundle lock --add-platform x86_64-linux
+bundle lock --add-platform ruby
 
 bundle install
-bundle exec rake tailwindcss:build
+
+bundle exec rails tailwindcss:install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
 bundle exec rails db:seed
