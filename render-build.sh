93f5753814b2ec6dc576a68c7d8959999e01b7af
nicktanda <nick.havilah@tanda.co>
2025-04-29 22:46:59 +1000

    get the fucking nokogiri gem working hopefully

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,14 @@
 # exit on error
 set -o errexit
 
-# Ensure platform is added BEFORE install
+# Ensure Ruby platform for native gems
 bundle lock --add-platform x86_64-linux
 
-# Avoid setting force_ruby_platform before installing native extensions
+# Force native extensions to be built (not precompiled ones)
+bundle config set force_ruby_platform true
+
 bundle install
 
-# Precompile assets and setup DB
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
