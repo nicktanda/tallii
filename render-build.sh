621176d976b68c2e5cb5e1672062192a0a643cea
nicktanda <nick.havilah@tanda.co>
2025-04-29 22:21:21 +1000

    precompile tailwind to see if this makes life good

--- render-build.sh
+++ render-build.sh
@@ -5,6 +5,7 @@ bundle config set force_ruby_platform true
 bundle lock --add-platform x86_64-linux
 
 bundle install
+bundle exec rake tailwindcss:build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
