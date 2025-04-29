fcb6f58b607d820a6c9d452e02049a6d6a774a6d
nicktanda <nick.havilah@tanda.co>
2025-04-29 22:43:22 +1000

    i wanna try this

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,14 @@
 # exit on error
 set -o errexit
 
-bundle config set force_ruby_platform true
+# Ensure platform is added BEFORE install
 bundle lock --add-platform x86_64-linux
 
+# Avoid setting force_ruby_platform before installing native extensions
 bundle install
+
+# Precompile assets and setup DB
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
 bundle exec rails db:seed
