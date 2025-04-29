77f312167dbc6759c2b83d2c308a5ba64361f735
nicktanda <nick.havilah@tanda.co>
2025-04-29 22:53:17 +1000

    fix the binaries maybe?

--- render-build.sh
+++ render-build.sh
@@ -1,22 +1,12 @@
 # exit on error
 set -o errexit
 
-# STEP 1: Add Linux platform to Gemfile.lock
 bundle lock --add-platform x86_64-linux
-
-# STEP 2: Force source builds of native extensions (Nokogiri, etc.)
 bundle config set force_ruby_platform true
 
-# STEP 3: Install gems
 bundle install
 
-# STEP 4: Manually install tailwindcss executable for Linux
-bin/rails tailwindcss:install
-
-# STEP 5: Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
-# STEP 6: Run DB tasks
 bundle exec rails db:migrate
 bundle exec rails db:seed
