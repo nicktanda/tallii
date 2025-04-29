a5a50f0e11f35365e68ca3434008e3e175f41eff
nicktanda <nick.havilah@tanda.co>
2025-04-29 22:39:31 +1000

    try this

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,6 @@ bundle config set force_ruby_platform true
 bundle lock --add-platform x86_64-linux
 
 bundle install
-bundle update --bundler
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
