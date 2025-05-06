9213d18a7cc163b4d331f625bc191885e3398fbc
nicktanda <nick.havilah@tanda.co>
2025-05-06 22:28:08 +1000

    try and get the stupid build steps running

--- render-build.sh
+++ render-build.sh
@@ -1,16 +1,22 @@
-# exit on error
 set -o errexit
 
+# Ensure correct Ruby platform is locked
 bundle lock --add-platform ruby
 
-# Ensure nokogiri builds against system libraries
+# Unset any previous force_ruby_platform
 bundle config unset force_ruby_platform
-bundle config set build.nokogiri --use-system-libraries
 
-bundle install
+# Force Nokogiri to build from source
+bundle config set --local build.nokogiri "--use-system-libraries"
 
+# Install gems, forcing native build
+bundle install --platform ruby
+
+# Build JS/CSS assets
 bundle exec rake tailwindcss:install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Setup DB
 bundle exec rails db:migrate
 bundle exec rails db:seed
