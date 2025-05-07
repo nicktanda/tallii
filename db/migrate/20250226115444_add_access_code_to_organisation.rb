096b06298f30b2f13b7760975d64dfa3abf7ee59
nicktanda <nick.havilah@tanda.co>
2025-05-07 21:41:27 +1000

    backfill access codes on organisation in case

--- db/migrate/20250226115444_add_access_code_to_organisation.rb
+++ db/migrate/20250226115444_add_access_code_to_organisation.rb
@@ -1,5 +1,9 @@
 class AddAccessCodeToOrganisation < ActiveRecord::Migration[7.0]
   def change
-    add_column :organisations, :access_code, :string, unique: true, null: false
+    add_column :organisations, :access_code, :string, unique: true
+    Organisation.all.each do |organisation|
+      organisation.generate_unique_code if organisation.access_code.blank?
+    end
+    change_column_null :organisations, :access_code, false
   end
 end
