0bdaf1103c0311f4e0e1a21e2e422ae3805dcc64
nicktanda <nick.havilah@tanda.co>
2025-10-23 23:13:16 +1000

    implement greyed out boxes for out of hours in the calendar

--- app/helpers/dashboard_controller_helper.rb
+++ app/helpers/dashboard_controller_helper.rb
@@ -7,4 +7,14 @@ module DashboardControllerHelper
     min = format('%02d',min)
     return "#{hrs}:#{min}"
   end
+
+  def is_in_opening_hours?(time, organisation, day)
+    opening_hours = organisation.opening_hours
+
+    return false if opening_hours[day]["closed"] == "true"
+
+    open = Time.parse(opening_hours[day]["open"])
+    close = Time.parse(opening_hours[day]["close"])
+    time >= open && time <= close
+  end
 end
