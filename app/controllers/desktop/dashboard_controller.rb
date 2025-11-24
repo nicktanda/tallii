68c4a185bf6d7964e833f2faed2af4664692c430
nicktanda <nick.havilah@tanda.co>
2025-11-24 23:34:34 +1000

    add some rescue clauses and things

--- app/controllers/desktop/dashboard_controller.rb
+++ app/controllers/desktop/dashboard_controller.rb
@@ -1,7 +1,12 @@
 module Desktop
   class DashboardController < DesktopController
     def index
-      @date = params[:date] ? Date.new(params[:date].split("-").first.to_i, params[:date].split("-").second.to_i, params[:date].split("-").last.to_i) : Date.today
+      @date = params[:date].present? ? Date.parse(params[:date]) : Date.today
+      @grooms = current_organisation.grooms.on_date(@date) + current_organisation.temporary_grooms.on_date(@date)
+      @daycare_visits = current_organisation.daycare_visits.on_date(@date) + current_organisation.temporary_daycare_visits.on_date(@date)
+      @bookings = (@grooms + @daycare_visits).sort_by(&:start_time)
+    rescue ArgumentError
+      @date = Date.today
       @grooms = current_organisation.grooms.on_date(@date) + current_organisation.temporary_grooms.on_date(@date)
       @daycare_visits = current_organisation.daycare_visits.on_date(@date) + current_organisation.temporary_daycare_visits.on_date(@date)
       @bookings = (@grooms + @daycare_visits).sort_by(&:start_time)
