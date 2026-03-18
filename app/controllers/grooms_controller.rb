087b0437e1f489f34975c26eb45fcef346fa56af
nicktanda <nick.havilah@tanda.co>
2026-03-18 18:29:18 +1000

    add calendar selector and multi select for grooms booking

--- app/controllers/grooms_controller.rb
+++ app/controllers/grooms_controller.rb
@@ -22,10 +22,15 @@ class GroomsController < ApplicationController
   end
 
   def create
-    groom = current_organisation.grooms.new(groom_params)
+    pet_ids = params[:groom][:pet_ids].presence || [params[:groom][:pet_id]].compact
+
+    if pet_ids.empty?
+      redirect_back fallback_location: new_groom_path, alert: 'Please select at least one pet'
+      return
+    end
 
     # Use cached count methods from Organisation model for consistency
-    if current_organisation.grooms_today_count >= current_organisation.maximum_daily_grooms
+    if current_organisation.grooms_today_count + pet_ids.length > current_organisation.maximum_daily_grooms
       if params[:groom][:origin] == "desktop"
         redirect_back fallback_location: desktop_grooms_new_path, alert: 'We are unable to take any extra grooms today, please rebook for another day'
       else
@@ -34,7 +39,7 @@ class GroomsController < ApplicationController
       return
     end
 
-    if current_organisation.grooms_this_week_count >= current_organisation.maximum_weekly_grooms
+    if current_organisation.grooms_this_week_count + pet_ids.length > current_organisation.maximum_weekly_grooms
       if params[:groom][:origin] == "desktop"
         redirect_back fallback_location: desktop_grooms_new_path, alert: 'We are unable to take any extra grooms this week, please rebook for another week'
       else
@@ -43,18 +48,31 @@ class GroomsController < ApplicationController
       return
     end
 
-    if groom.save
-      groom.images.create!(name: "test_image", image: params[:groom][:image])
-      if params[:groom][:origin] == "desktop"
-        redirect_to desktop_grooms_path, notice: 'Groom appointment saved'
+    created_grooms = []
+    errors = []
+
+    pet_ids.each do |pet_id|
+      groom = current_organisation.grooms.new(groom_params_without_pets.merge(pet_id: pet_id))
+      if groom.save
+        groom.images.create!(name: "test_image", image: params[:groom][:image]) if params[:groom][:image].present?
+        created_grooms << groom
       else
-        redirect_to grooms_path, notice: 'Groom appointment saved'
+        errors << "Failed to create groom for pet #{pet_id}"
+      end
+    end
+
+    if errors.empty?
+      notice = pet_ids.length > 1 ? "#{pet_ids.length} groom appointments saved" : 'Groom appointment saved'
+      if params[:groom][:origin] == "desktop"
+        redirect_to desktop_grooms_path, notice: notice
+      else
+        redirect_to grooms_path, notice: notice
       end
     else
       if params[:groom][:origin] == "desktop"
-        redirect_back fallback_location: desktop_grooms_new_path, alert: 'Invalid groom information'
+        redirect_back fallback_location: desktop_grooms_new_path, alert: 'Some groom appointments could not be created'
       else
-        redirect_back fallback_location: new_groom_path, alert: 'Invalid groom information'
+        redirect_back fallback_location: new_groom_path, alert: 'Some groom appointments could not be created'
       end
     end
   end
@@ -82,6 +100,10 @@ class GroomsController < ApplicationController
   private
 
   def groom_params
-    params.require(:groom).permit(:date, :start_time, :end_time, :notes, :pet_id, :last_groom, :employee_id, :status)
+    params.require(:groom).permit(:date, :start_time, :end_time, :notes, :pet_id, :last_groom, :employee_id, :status, pet_ids: [])
+  end
+
+  def groom_params_without_pets
+    params.require(:groom).permit(:date, :start_time, :end_time, :notes, :last_groom, :employee_id, :status)
   end
 end
